d9d26c62f5de25bb2bf670d7d5534f2373c4797d
Max Mynter <maxmynter@me.com>
2024-01-18 12:29:16 +0100

    Update Landing page

--- src/app/texts/landing.tsx
+++ src/app/texts/landing.tsx
@@ -6,8 +6,8 @@ export const langdingPageText = `Greentexts are a distinctive form of storytelli
 > train small llms instead
 > large small large language models next?
 
-I've trained multiple small (1M- 125M parameter) LLMs to create stories in the greentext style. Interestingly, the bigger ones do not automatically perform better. You can tune a lot of the performance with the training data quantity specificity.
+I've fine-tuned multiple small (1M- 125M parameters) LLMs to create stories in the greentext style. Interestingly, the bigger ones do not automatically perform better. You can tune a lot of the performance with the training data quantity specificity.
 
-Click "Generate (model, dataset)" below to try how the models and training procedures compare. You can indicate the quality of each generation.
+Click "Generate (model name, dataset)" below to try how the models and training procedures compare and can indicate the quality of each generation.
 The first generation of a model might take a while, since I have to spin up the inference server.
 See the links above for details on models, datasets, and the project in general.`
